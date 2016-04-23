8f3e60411cd9b17cb5eb68c745cce56aa59a39d8
John <jomcode@gmail.com>
2016-04-23 00:30:53 -0500

    software parsing test passing

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const requestHeaderParser = (headers) => {
 
   const ipaddress = headers['host'].slice(0);
   const language = languageRe.exec(headers['accept-language'].slice(0))[0];
+  const software = softwareRe.exec(headers['user-agent'].slice(0))[0];
 
-  return { ipaddress, language, software: '' };
+  return { ipaddress, language, software };
 };
 
 module.exports.requestHeaderParser = requestHeaderParser;
--- test/requestheaderparser.test.js
+++ test/requestheaderparser.test.js
@@ -5,7 +5,8 @@ const requestHeaderParser = require('../index').requestHeaderParser;
 describe('request header parser microservice', () => {
   const fakeHeaders = {
     host: '127.0.0.1',
-    'accept-language': 'en-US,en;q=0.5'
+    'accept-language': 'en-US,en;q=0.5',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'
   };
 
   it('returns object with ipaddress, language, and software properties', () => {
@@ -23,5 +24,10 @@ describe('request header parser microservice', () => {
   it('properly parses the language', () => {
     const result = requestHeaderParser(fakeHeaders);
     expect(result).to.have.property('language', 'en-US');
+  });
+
+  it('properly parses the software', () => {
+    const result = requestHeaderParser(fakeHeaders);
+    expect(result).to.have.property('software', 'Windows NT 6.3; WOW64; rv:45.0');
   });
 });
